3ff0de09ab8077004f1a32add1471805e4744d3a
Ashpex <38400042+Ashpex@users.noreply.github.com>
2021-01-29 16:07:59 +0700

    Update arch_package.sh

--- arch_package.sh
+++ arch_package.sh
@@ -6,6 +6,11 @@ cd ..
 rm -rf yay
 ### Install i3 ###
 
-sudo pacman -S i3-gaps dunst dmenu picom feh mpd mpv ranger rofi scrot neovim xorg xorg-server pulseaudio pulseaudio-alsa alsa-utils nemo termite firefox git zathura
+sudo pacman -S i3-gaps dunst dmenu picom feh mpd mpv ranger rofi scrot neovim xorg xorg-server pulseaudio pulseaudio-alsa alsa-utils nemo firefox git zathura
 yay polybar
 yay ranger-git
+
+### Install from pkglist
+git clone https://github.com/Ashpex/pkglist.git
+sudo pacman -S - < pkglist.txt
+sudo pacman -S $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
