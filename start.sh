8b3abef520e4f86a0c26696a424e813c9f550ac9
ashpex <ashpex@pm.me>
2021-10-05 15:58:21 +0700

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xrandr --output HDMI-2 --mode 2560x1440 --rate 59.95 --primary --left-of eDP-1  --output eDP-1 --mode 1920x1080
-feh --bg-fill ~/Downloads/wallpapers/car.jpg
+feh --bg-fill ~/Downloads/wallpapers/midnight.png
 pkill conky
 conky -d
