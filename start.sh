86b424fe92138bf5a0ffa0a0b5f07cd57a9662d5
ashpex <ashpex@pm.me>
2022-01-07 01:43:53 +0700

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 xrandr --output HDMI-2 --mode 2560x1440 --rate 59.95 --primary --left-of eDP-1  --output eDP-1 --mode 1920x1080
 feh --bg-fill ~/Downloads/wallpapers/midnight.png
+wg-quick down WireGuard
 pkill conky
 conky -d
